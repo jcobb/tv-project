7f8f5000ded126b5bda3981b1cc3a59f99bfd021
John Cobb <cobb@yahoo-inc.com>
2015-03-12 20:16:45 +1100

    [CONTROLLERS] added basic routes for login/logout/register

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,48 @@
 var express = require('express')
-  , router = express.Router()
+  , router = express.Router();
+
+var passport = require('passport');
+var Account = require('../models/account');
 
 router.use('/search', require('./search'));
 
-router.get('/', function(req, res) {
-  res.render('index')
-})
+router.get('/', function (req, res) {
+    res.render('index', { user : req.user });
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+router.post('/register', function(req, res) {
+
+
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { account : account });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/ping', function(req, res){
+    res.status(req.status).send("pong!", 200);
+});
 
 module.exports = router
