5b9549d1b854cdabb48a4c6956348baf6b44aa60
John Cobb <cobb@yahoo-inc.com>
2015-03-12 20:17:25 +1100

    [AUTH] Added middleware and modules for basic authentication

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var dbConfig = require('./models/db-config');
+var logger = require('morgan');
 
 app.engine('jade', require('jade').__express);
 app.set('view engine', 'jade');
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 app.use(require('./controllers'));
 
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+mongoose.connect(dbConfig.url);
+
 app.listen(3000, function() {
-  console.log('Listening on port 3000...');
+    console.log('Listening on port 3000...');
 });
